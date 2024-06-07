1f5e9f6a1c2df7dc237589c5c2b41dae693ac342
m0emEn <moemensfaxi10@gmail.com>
2024-06-07 16:46:07 +0100

    email sender

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,42 @@
 import project from "../assets/project.svg";
 import client from "../assets/client.svg";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import emailjs from "emailjs-com";
 import { useAnimation, motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 import CountUp from "react-countup";
 
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    number: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(
+      "service_o3mkpws",
+      "template_odhhdf8",
+      e.target,
+      "eA4Fwo5prZOw7iNwl"
+    );
+
+    setFormData({
+      name: "",
+      email: "",
+      number: "",
+      message: "",
+    });
+  };
+
   const variants = {
     visible: {
       opacity: 1,
@@ -15,13 +46,16 @@ const Contact = () => {
     },
     hidden: { opacity: 0, y: 20, skewY: 1 },
   };
+
   const controls = useAnimation();
   const [ref, inView] = useInView();
+
   useEffect(() => {
     if (inView) {
       controls.start("visible");
     }
   }, [controls, inView]);
+
   return (
     <div className="contact">
       <div className="description">
@@ -61,17 +95,48 @@ const Contact = () => {
           initial="hidden"
           variants={variants}
         >
-          GET IN TOUCHE
+          GET IN TOUCH
         </motion.h1>
         <p>
           Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
           auting fugit sed thisnquia consequuntur magni dolores eos designer
         </p>
-        <input type="text" placeholder="Your Name" />
-        <input type="email" placeholder="Your Email" autoComplete={"false"} />
-        <input type="number" placeholder="Your Number" />
-        <textarea placeholder="Message" />
-        <button className="mainBtn">Submit Now</button>
+        <form onSubmit={handleSubmit} className="form">
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            placeholder="Your Name"
+            required
+          />
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            autoComplete="false"
+            placeholder="Your Email"
+            required
+          />
+          <input
+            type="text"
+            name="number"
+            value={formData.number}
+            onChange={handleChange}
+            placeholder="Your Number"
+          />
+          <textarea
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+            placeholder="Message"
+            required
+          />
+          <button className="mainBtn" type="submit">
+            Submit Now
+          </button>
+        </form>
       </div>
     </div>
   );
